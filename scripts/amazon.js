3961d39031e2f4d8b08b542075b3aab5b73113e6
eironjim13 <lanzaderas.eiron13@gmail.com>
2026-01-20 21:02:52 +0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -85,7 +85,14 @@ document.querySelector('.js-products-grid').
           });
         }
 
-        
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+          .innerHTML = cartQuantity;
+
       });
     });
