d6a3a9d5e08e9393a1ed6afbe560a82da5f91dc8
eironjim13 <lanzaderas.eiron13@gmail.com>
2026-01-21 10:41:26 +0800

    added popup to the add-to-cart button w/ delay

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -44,7 +44,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -65,6 +65,8 @@ document.querySelector('.js-products-grid').
 
   document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
+      let addedMessageTimeoutId;
+
       button.addEventListener('click', () => {
         const productId = button.dataset.productId;
 
@@ -75,13 +77,18 @@ document.querySelector('.js-products-grid').
             matchingItem = item;
           }
         });
+        
+        const quantitySelector = document.querySelector(
+          `.js-quantity-selector-${productId}`
+        );
+        const quantity = Number(quantitySelector.value);
 
         if (matchingItem) {
-          matchingItem.quantity += 1;
+          matchingItem.quantity += quantity;
         } else {
           cart.push({
             productId: productId,
-            quantity: 1
+            quantity: quantity
           });
         }
 
@@ -94,5 +101,20 @@ document.querySelector('.js-products-grid').
         document.querySelector('.js-cart-quantity')
           .innerHTML = cartQuantity;
 
+        const addedMessage = document.querySelector(
+          `.js-added-to-cart-${productId}`
+        );
+
+        addedMessage.classList.add('added-to-cart-visible');
+        
+        if (addedMessageTimeoutId) {
+          clearTimeout(addedMessageTimeoutId);
+        }
+
+        const timeoutId = setTimeout(() => {
+          addedMessage.classList.remove('added-to-cart-visible');
+        }, 2000);
+
+        addedMessageTimeoutId = timeoutId;
       });
     });
