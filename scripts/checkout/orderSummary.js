6c27ab2d6c9c8b2c9a2959ae473cdbdc9f46acee
eironjim13 <lanzaderas.eiron13@gmail.com>
2026-01-22 20:43:45 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -9,10 +9,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -140,12 +137,13 @@ export function renderOrderSummary() {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-
+        
         const container = document.querySelector(
           `.js-cart-item-container-${productId}`
           );
         container.remove();
-
+        
+        renderPaymentSummary();
         updateCartQuantity();
       });
     });
@@ -156,6 +154,7 @@ export function renderOrderSummary() {
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
 
